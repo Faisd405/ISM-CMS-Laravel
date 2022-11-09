4b7495f831745ce4d35289bdd0308dae6cc3ddb4
faisd405 <faisd405@gmail.com>
2022-11-09 09:59:03 +0700

    perkondisian support

--- resources/views/layouts/frontend/includes/chat-widget.blade.php
+++ resources/views/layouts/frontend/includes/chat-widget.blade.php
@@ -9,60 +9,68 @@
                     <div>@lang('text.text_support')</div>
                 </li>
                 <li class="wa-body">
-                    <div class="wa-item">
-                        <a href="https://web.whatsapp.com/send?phone={{ config('cmsConfig.general.support_phone') }}text={{ config('cmsConfig.general.support_text') }}"
-                            target="_blank" class="wa-link dropdown-link d-flex align-items-center">
-                            <div class="wa-icon ratio ratio-1x1 rounded-circle overflow-hidden me-3">
-                                <div class="thumb d-flex align-items-center justify-content-center">
-                                    @if (last(explode('/',config('cmsConfig.file.support_image'))) != null)
-                                        <img src="{{ config('cmsConfig.file.support_image') }}" alt=""
-                                            class="thumb">
-                                    @endif
-                                    <i class="fa-brands fa-whatsapp fs-20"></i>
+                    @if (config('cmsConfig.general.support_phone') && config('cmsConfig.general.support_name'))
+                        <div class="wa-item">
+                            <a href="https://web.whatsapp.com/send?phone={{ config('cmsConfig.general.support_phone') }}text={{ config('cmsConfig.general.support_text') ?? '' }}"
+                                target="_blank" class="wa-link dropdown-link d-flex align-items-center">
+                                <div class="wa-icon ratio ratio-1x1 rounded-circle overflow-hidden me-3">
+                                    <div class="thumb d-flex align-items-center justify-content-center">
+                                        @if (last(explode('/', config('cmsConfig.file.support_image'))) != null)
+                                            <img src="{{ config('cmsConfig.file.support_image') }}" alt=""
+                                                class="thumb">
+                                        @endif
+                                        <i class="fa-brands fa-whatsapp fs-20"></i>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="wa-info flex-grow-1">
-                                <div class="wa-title fs-12 text-muted">@lang('text.support_1')</div>
-                                <div class="wa-name text-dark">{{ config('cmsConfig.general.support_name') }}</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="wa-item">
-                        <a href="https://web.whatsapp.com/send?phone={{ config('cmsConfig.general.support_phone_2') }}text={{ config('cmsConfig.general.support_text_2') }}"
-                            target="_blank" class="wa-link dropdown-link d-flex align-items-center">
-                            <div class="wa-icon ratio ratio-1x1 rounded-circle overflow-hidden me-3">
-                                <div class="thumb d-flex align-items-center justify-content-center">
-                                    @if (last(explode('/',config('cmsConfig.file.support_image_2'))) != null)
-                                        <img src="{{ config('cmsConfig.file.support_image_2') }}" alt=""
-                                            class="thumb">
-                                    @endif
-                                    <i class="fa-brands fa-whatsapp fs-20"></i>
+                                <div class="wa-info flex-grow-1">
+                                    <div class="wa-title fs-12 text-muted">@lang('text.support_1')</div>
+                                    <div class="wa-name text-dark">{{ config('cmsConfig.general.support_name') }}</div>
                                 </div>
-                            </div>
-                            <div class="wa-info flex-grow-1">
-                                <div class="wa-title fs-12 text-muted">@lang('text.support_2')</div>
-                                <div class="wa-name text-dark">{{ config('cmsConfig.general.support_name_2') }}</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="wa-item">
-                        <a href="https://web.whatsapp.com/send?phone={{ config('cmsConfig.general.support_phone_3') }}text={{ config('cmsConfig.general.support_text_3') }}"
-                            target="_blank" class="wa-link dropdown-link d-flex align-items-center">
-                            <div class="wa-icon ratio ratio-1x1 rounded-circle overflow-hidden me-3">
-                                <div class="thumb d-flex align-items-center justify-content-center">
-                                    @if (last(explode('/',config('cmsConfig.file.support_image_3'))) != null)
-                                        <img src="{{ config('cmsConfig.file.support_image_3') }}" alt=""
-                                            class="thumb">
-                                    @endif
-                                    <i class="fa-brands fa-whatsapp fs-20"></i>
+                            </a>
+                        </div>
+                    @endif
+                    @if (config('cmsConfig.general.support_phone_2') && config('cmsConfig.general.support_name_2'))
+                        <div class="wa-item">
+                            <a href="https://web.whatsapp.com/send?phone={{ config('cmsConfig.general.support_phone_2') }}text={{ config('cmsConfig.general.support_text_2') ?? '' }}"
+                                target="_blank" class="wa-link dropdown-link d-flex align-items-center">
+                                <div class="wa-icon ratio ratio-1x1 rounded-circle overflow-hidden me-3">
+                                    <div class="thumb d-flex align-items-center justify-content-center">
+                                        @if (last(explode('/', config('cmsConfig.file.support_image_2'))) != null)
+                                            <img src="{{ config('cmsConfig.file.support_image_2') }}" alt=""
+                                                class="thumb">
+                                        @endif
+                                        <i class="fa-brands fa-whatsapp fs-20"></i>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="wa-info flex-grow-1">
-                                <div class="wa-title fs-12 text-muted">@lang('text.support_3')</div>
-                                <div class="wa-name text-dark">{{ config('cmsConfig.general.support_name_3') }}</div>
-                            </div>
-                        </a>
-                    </div>
+                                <div class="wa-info flex-grow-1">
+                                    <div class="wa-title fs-12 text-muted">@lang('text.support_2')</div>
+                                    <div class="wa-name text-dark">{{ config('cmsConfig.general.support_name_2') }}
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endif
+                    @if (config('cmsConfig.general.support_phone_3') && config('cmsConfig.general.support_name_3'))
+                        <div class="wa-item">
+                            <a href="https://web.whatsapp.com/send?phone={{ config('cmsConfig.general.support_phone_3') }}text={{ config('cmsConfig.general.support_text_3') ?? '' }}"
+                                target="_blank" class="wa-link dropdown-link d-flex align-items-center">
+                                <div class="wa-icon ratio ratio-1x1 rounded-circle overflow-hidden me-3">
+                                    <div class="thumb d-flex align-items-center justify-content-center">
+                                        @if (last(explode('/', config('cmsConfig.file.support_image_3'))) != null)
+                                            <img src="{{ config('cmsConfig.file.support_image_3') }}" alt=""
+                                                class="thumb">
+                                        @endif
+                                        <i class="fa-brands fa-whatsapp fs-20"></i>
+                                    </div>
+                                </div>
+                                <div class="wa-info flex-grow-1">
+                                    <div class="wa-title fs-12 text-muted">@lang('text.support_3')</div>
+                                    <div class="wa-name text-dark">{{ config('cmsConfig.general.support_name_3') }}
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endif
                 </li>
             </ul>
         </div>
